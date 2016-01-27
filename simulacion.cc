5687c345349d5f032c9c20fe3b76a21ddf92426e
Alejandro Rodríguez Calzado <alerguez94@gmail.com>
2016-01-27 19:17:39 +0100

    añadida simulacion para VOIP

--- simulacion.cc
+++ simulacion.cc
@@ -18,7 +18,9 @@
 
 void simulacionCSMA_FTP (uint32_t nCsma, Time retardoProp, DataRate capacidad, 
         uint32_t tamPaquete);
-
+//Simulación simple para el servicio VoIP usando CSMA
+void
+simulacionCSMA (uint32_t nCsma, Time ton, Time toff, uint32_t sizePkt, DataRate dataRate);
 
 
 
@@ -34,8 +36,9 @@ main (int argc, char *argv[])
 	GlobalValue::Bind("ChecksumEnabled", BooleanValue(true));
 	Time::SetResolution (Time::NS);
 
-simulacionCSMA_FTP ((uint32_t) 50, Time("6560ns"), DataRate("10Mbps"), (uint32_t)40);
-
+//simulacionCSMA_FTP ((uint32_t) 50, Time("6560ns"), DataRate("10Mbps"), (uint32_t)40);
+//Simulación simple para el servicio VoIP usando CSMA
+simulacionCSMA (30, Time("0.150s"), Time("0.650s"), (uint32_t)40, DataRate("64kbps"));
 
 /*
     //Variable para el calculo del intervalo de confianza.
@@ -231,4 +234,102 @@ simulacionCSMA_FTP (uint32_t nCsma, Time retardoProp, DataRate capacidad, uint32
     NS_LOG_ERROR(observadorCSMA.GetMediaTiempos());
     //NS_LOG_ERROR(observadorCSMA[157].GetMediaIntentos());
     //NS_LOG_ERROR(observadorCSMA[157].GetPorcentajePktsRetrasados ());
+}
+
+
+//Simulación simple para el servicio VoIP usando CSMA
+void
+simulacionCSMA (uint32_t nCsma, Time ton, Time toff, uint32_t sizePkt, DataRate dataRate)
+{
+NS_LOG_FUNCTION(nCsma << ton << toff << sizePkt << dataRate);
+
+
+  // Nodos que pertenecen a la red de área local
+  // Como primer nodo añadimos el sumidero.
+  NodeContainer csmaNodes;
+  csmaNodes.Create (nCsma);
+
+  // Instalamos el dispositivo de red en los nodos de la LAN
+  CsmaHelper csma;
+  NetDeviceContainer csmaDevices;
+  csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
+  csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
+  csmaDevices = csma.Install (csmaNodes);
+
+  // Instalamos la pila TCP/IP en todos los nodos
+  InternetStackHelper stack;
+  stack.Install (csmaNodes);
+
+  // Asignamos direcciones a cada una de las interfaces
+  Ipv4AddressHelper address;
+  Ipv4InterfaceContainer csmaInterfaces;
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  csmaInterfaces = address.Assign (csmaDevices);
+
+  // Calculamos las rutas del escenario. 
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  
+  // Establecemos un sumidero para los paquetes en el puerto 9 del nodo
+  //     aislado del enlace punto a punto
+  uint16_t port = 9;
+  PacketSinkHelper sink ("ns3::UdpSocketFactory",
+                         Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
+  ApplicationContainer app = sink.Install (csmaNodes.Get (0));
+
+  // Instalamos un cliente OnOff en uno de los equipos de la red de área local
+  OnOffHelper VoIP ("ns3::UdpSocketFactory",
+                        Address (InetSocketAddress (csmaInterfaces.GetAddress (0), port)));
+
+  // Creamos las variables aleatorias para los tiempos de on y off
+  Ptr<ExponentialRandomVariable> tonExponencial = CreateObject<ExponentialRandomVariable> ();
+  Ptr<ExponentialRandomVariable> toffExponencial = CreateObject<ExponentialRandomVariable> ();
+  // Especificamos las medias de estas variables
+  tonExponencial->SetAttribute("Mean", DoubleValue(ton.GetDouble()/1e6));
+  toffExponencial->SetAttribute("Mean", DoubleValue(toff.GetDouble()/1e6));
+  // Asociamos las variables aleatorias al cliente OnOff   
+  VoIP.SetAttribute("OnTime", PointerValue(tonExponencial));
+  VoIP.SetAttribute("OffTime", PointerValue(toffExponencial));
+  VoIP.SetAttribute("PacketSize", UintegerValue(sizePkt));
+  VoIP.SetAttribute("DataRate", DataRateValue(dataRate));
+  
+  NodeContainer clientes;
+
+  for (uint32_t i = 1; i < nCsma; i++)
+    clientes.Add(csmaNodes.Get(i));
+
+  //Instalamos la aplicación On/Off en todos y cada uno de los nodos de la red de área local.
+  ApplicationContainer clientApps = VoIP.Install (clientes);
+  clientApps.Start (Seconds (2.0));
+  clientApps.Stop (Seconds (10.0));
+
+  // Activamos las trazas pcap en las dos interfaces del nodo de enlace
+  //csma.EnablePcap ("practica06", csmaDevices.Get (0), true);
+
+  //Objeto observador para obtener los resultados de las simulaciones.
+  ObservadorCSMA observadorCSMA;
+
+  for (uint32_t i = 0; i < clientApps.GetN(); i++)
+    //Conectamos las trazas al observador para todos los clientes.
+    clientApps.Get(i)->GetObject<OnOffApplication>()->TraceConnectWithoutContext ("Tx", MakeCallback(&ObservadorCSMA::PktGenerado, &observadorCSMA));
+  
+  //Conectamos la traza del sumidero al observador.
+  app.Get(0)->GetObject<PacketSink>()->TraceConnectWithoutContext ("Rx", MakeCallback(&ObservadorCSMA::PktRecibido, &observadorCSMA));
+    
+  for (uint32_t j = 1; j < nCsma; j++)
+    csmaDevices.Get(j)->TraceConnectWithoutContext ("PhyTxDrop", MakeCallback(&ObservadorCSMA::EnvioDescartado, &observadorCSMA));
+
+
+  observadorCSMA.SetTamPkt(sizePkt);
+
+
+  // Lanzamos la simulación
+  Simulator::Run ();
+  Simulator::Destroy ();
+
+  double retardo = observadorCSMA.GetMediaTiempos()/1e6;
+  double pktsCorrectos = observadorCSMA.GetPorcentajePktsPerdidos();
+
+  NS_LOG_INFO("Retardo de transmisión medio: " <<  retardo << "ms");
+  NS_LOG_INFO("Porcentaje de paquetes perdidos: " << pktsCorrectos << "%");
 }
