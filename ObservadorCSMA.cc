e2998280eb66778f0b900ee1c5afea6a563051e8
Alejandro Rodríguez Calzado <alerguez94@gmail.com>
2016-01-27 21:34:17 +0100

    Se ha quitado el sobrante

--- ObservadorCSMA.cc
+++ ObservadorCSMA.cc
@@ -14,149 +14,11 @@ ObservadorCSMA::ObservadorCSMA ()
 
   m_nodo = 0;
   m_tamPkt = 0;
-  //m_numPktsRetrasados = 0;
-  //m_numIntentos = 0;
   m_numPeticionesTx = 0;
   m_numPktsPerdidos = 0;
-  //m_tiempoInicial = 0;
   m_retardo = Time(0);
-  //m_pktRetrasado = false;
-  //m_acIntentos.Reset();
   m_acTiempos.Reset();
 }
-
-/*//Funcion que maneja la traza MacTxBackoff
-void
-ObservadorCSMA::EnvioRetrasado (Ptr<const Packet> paquete)
-{
-  NS_LOG_FUNCTION (paquete);
-
-
-  // Obtengo una copia del paquete
-  Ptr<Packet> copia = paquete->Copy();
-
-  EthernetHeader header;
-  // Quitamos la cabecera del paquete y la guardamos en header.
-  copia->RemoveHeader (header);
-
-  // Comprobamos que sea un paquete ip (contiene el paquete eco).
-  if (header.GetLengthType() == 0x0800)
-  {
-    NS_LOG_DEBUG ("NODO " << m_nodo <<"-> Se ha retrasado el envio de un paquete debido al proceso de backoff.");
-    m_numIntentos ++;
-    m_pktRetrasado = true;
-    NS_LOG_DEBUG ("Numero de intentos: " << m_numIntentos);
-  }
-}*/
-
-//Funcion que maneja la traza PhyTxDrop
-void
-ObservadorCSMA::EnvioDescartado (Ptr<const Packet> paquete)
-{
-  NS_LOG_FUNCTION (paquete);
-
-  // Obtengo una copia del paquete
-  Ptr<Packet> copia = paquete->Copy();
-
-  EthernetHeader header;
-  // Quitamos la cabecera del paquete y la guardamos en header.
-  copia->RemoveHeader (header);
-
-  // Comprobamos que sea un paquete ip (contiene el paquete eco).
-  //if (header.GetLengthType() == 0x0800)
-  //{
-    NS_LOG_DEBUG("NODO " << m_nodo <<"-> Se ha descartado el envio de un paquete porque ha llegado al maximo numero de intentos.");
-    //m_numIntentos = 0;
-    //m_pktRetrasado = false;
-    //Aumentamos el numero de paquetes perdidos
-    m_numPktsPerdidos++;
-  //}
-}
-/*//Funcion que maneja la traza PhyTxEnd
-void
-ObservadorCSMA::EnvioTerminado (Ptr<const Packet> paquete)
-{
-  NS_LOG_FUNCTION (paquete);
-
-
-  // Obtengo una copia del paquete
-  Ptr<Packet> copia = paquete->Copy();
-
-  EthernetHeader header;
-  // Quitamos la cabecera del paquete y la guardamos en header.
-  copia->RemoveHeader (header);
-
-  // Comprobamos que sea un paquete ip (contiene el paquete eco).
-  if (header.GetLengthType() == 0x0800)
-  {
-    m_numIntentos ++;
-    NS_LOG_DEBUG ("NODO " << m_nodo <<"-> Se ha terminado el proceso de envio de un paquete en el nodo.");
-    NS_LOG_DEBUG ("Numero de intentos: " << m_numIntentos);
-    m_acIntentos.Update(m_numIntentos);
-    m_numIntentos = 0;
-
-    if (m_pktRetrasado == true)
-    {
-      m_numPktsRetrasados++;
-      m_pktRetrasado = false;
-    }
-  }
-}*/
-
-/*
-//Funcion que maneja la traza MacTx
-void 
-ObservadorCSMA::OrdenEnvio (Ptr<const Packet> paquete)
-{
-  NS_LOG_FUNCTION(paquete);
-  NS_LOG_ERROR("MACTX:" << paquete->GetUid());
-  NS_LOG_ERROR("Nodo: " << m_nodo);
-  // Obtengo una copia del paquete
-  Ptr<Packet> copia = paquete->Copy();
-
-  EthernetHeader header;
-  // Quitamos la cabecera del paquete y la guardamos en header.
-  copia->RemoveHeader (header);
-
-  // Comprobamos que sea un paquete ip.
-  if (header.GetLengthType() == 0x0800)
-  {
-    NS_LOG_DEBUG("NODO " << m_nodo <<"-> Se ha recibido la orden de envio de un paquete ECO.");
-
-    //Aumentamos el numero de peticiones de transmision
-    m_numPeticionesTx++;
-  }
-
-}*/
-
-/*
-//Funcion que maneja la traza MacRx
-void
-ObservadorCSMA::OrdenPktDisponible (Ptr<const Packet> paquete)
-{
-  NS_LOG_FUNCTION(paquete);
-
-  int64_t transcurrido = 0.0;
-
-  // Obtengo una copia del paquete
-  Ptr<Packet> copia = paquete->Copy();
-
-  EthernetHeader header;
-  // Quitamos la cabecera del paquete y la guardamos en header.
-  copia->RemoveHeader (header);
-
-  // Comprobamos que sea un paquete ip (contiene el paquete eco).
-  if (header.GetLengthType() == 0x0800)
-  {
-    NS_LOG_DEBUG("NODO " << m_nodo <<"-> Dispone de un paquete ECO para su entrega al nivel de aplicaion.");
-
-    transcurrido = Simulator::Now().GetNanoSeconds() - m_tiempoInicial;
-    m_acTiempos.Update(transcurrido);
-
-    NS_LOG_DEBUG("Tiempo de ECO: " << (double)transcurrido/1e6 << " ms");
-  }
-}
-*/
 
 
 //Funcion que maneja la traza Tx
@@ -167,12 +29,9 @@ ObservadorCSMA::PktGenerado (Ptr<const Packet> paquete)
 
   NS_LOG_DEBUG ("Se ha generado un nuevo paquete y va a ser enviado.");
 
-NS_LOG_ERROR("TX:" << paquete->GetUid());
-NS_LOG_ERROR("Nodo: " << m_nodo);
   //Almacenamos el tiempo inicial.
   m_tiemposIniciales[paquete->GetUid()] = Simulator::Now();
   m_numPeticionesTx++;
-  NS_LOG_ERROR("tam_estruct: " << m_tiemposIniciales.size());
 }
 
 
@@ -181,13 +40,10 @@ void
 ObservadorCSMA::PktRecibido (Ptr<const Packet> paquete, const Address &)
 {
   NS_LOG_FUNCTION (paquete);
-NS_LOG_ERROR("RX:" << paquete->GetUid());
-NS_LOG_ERROR("Nodo: " << m_nodo);
+
   //Buscamos en la estructura de timepos iniciales el correspondiente a este paquete.
   m_iterador = m_tiemposIniciales.find (paquete->GetUid());
 
-  bool result = m_iterador != m_tiemposIniciales.end();
-NS_LOG_ERROR(result);
   if (m_iterador != m_tiemposIniciales.end())
   {
     //Si encontramos el paquete en la estructura...
@@ -206,32 +62,7 @@ NS_LOG_ERROR(result);
     NS_LOG_WARN("No se ha encontrado el paquete recibido en la estructura de tiempos iniciales."); 
 }
 
-/*
-//Devuelve el número medio de intentos necesarios para 
-//transmitir efectivamente un paquete del nodo asociado 
-//al objeto ObservadorCSMA.
-double 
-ObservadorCSMA::GetMediaIntentos ()
-{
-  NS_LOG_FUNCTION_NOARGS ();
 
-  double result = -1;
-
-  //Comprobamos que se haya realizado correctamente algun envio
-  if (m_acIntentos.Count() > 0)
-    result = m_acIntentos.Mean();
-
-  return result;
-}
-*/
-/*
-//Devuelve el porcentaje de pkts retrasados antes de ser enviados.  
-double
-ObservadorCSMA::GetPorcentajePktsRetrasados ()
-{
-  return 100*(double)m_numPktsRetrasados/(double)m_numPeticionesTx;
-}
-*/
 
 //Devuelve el tiempo medio de ECO del nodo asociado al
 //objeto ObservadorCSMA. Se devuelve en ns.
@@ -260,7 +91,7 @@ ObservadorCSMA::GetPorcentajePktsPerdidos()
   double result = -1;
 
   if (m_numPeticionesTx > 0)
-    result = 100*(double)m_numPktsPerdidos / (double)m_numPeticionesTx;
+    result = 100*(double)m_tiemposIniciales.size() / (double)m_numPeticionesTx;
 
   return result;
 }
@@ -283,22 +114,6 @@ ObservadorCSMA::GetTasaMedia () {
 
   return result;
 }
-
-/*
-//Funcion para resetear variables y objetos Average, pero
-//no resetea la variable m_nodo ya que el ObservadorCSMA
-//seguira asociado al mismo nodo.
-void
-ObservadorCSMA::Reset()
-{
-  m_numIntentos = 0;
-  m_numPeticionesTx = 0;
-  //m_numPktsPerdidos = 0;
-  m_tiempoInicial = 0;
-  m_acIntentos.Reset();
-  m_acTiempos.Reset();
-}
-*/
 
 //Funcion para guardar en la variable m_nodo el identificador
 //del nodo al que esta asociado el ObservadorCSMA. Esto se usara 
--- simulacion.cc
+++ simulacion.cc
@@ -11,24 +11,15 @@
 #include "ns3/gnuplot.h"
 #include "ObservadorCSMA.h"
 
-#define NUM_SIMULACIONES 10
-#define TSTUDENT10 2.2622
-#define DNI0 5
-#define DNI1 1
 
-
-void simulacionCSMA_FTP (uint32_t nCsma, Time retardoProp, DataRate capacidad, 
-        uint32_t tamPaquete);
 //Simulación simple para el servicio VoIP usando CSMA
 void
 simulacionCSMA (uint32_t nCsma, Time ton, Time toff, uint32_t sizePkt, DataRate dataRate, double prob_error_pkt);
 
 
-
-
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("FTP-CSMA");
+NS_LOG_COMPONENT_DEFINE ("VOIP-CSMA");
 
 
 int
@@ -37,205 +28,11 @@ main (int argc, char *argv[])
 	GlobalValue::Bind("ChecksumEnabled", BooleanValue(true));
 	Time::SetResolution (Time::US);
 
-//simulacionCSMA_FTP ((uint32_t) 50, Time("6560ns"), DataRate("10Mbps"), (uint32_t)40);
-//Simulación simple para el servicio VoIP usando CSMA
-simulacionCSMA (10, Time("0.150s"), Time("0.650s"), (uint32_t)40, DataRate("64kbps"), 0.2);
 
-/*
-    //Variable para el calculo del intervalo de confianza.
-    double z = 0.0;
+simulacionCSMA (250, Time("0.150s"), Time("0.650s"), (uint32_t)40, DataRate("64kbps"), 1e-4);
 
-	uint32_t nCsma = 10-DNI0/2;
-    Time retardoProp = Time("6560ns");
-    DataRate capacidad = DataRate("100Mbps");
-    uint32_t tamPaquete = 500+100*DNI1;
-    Time intervalo = Time("1s");
-
-	CommandLine cmd;
-	cmd.AddValue ("nCsma", "Número de nodos de la red local", nCsma);
-    cmd.AddValue ("retardoProp", "Retardo de propagación en el bus", retardoProp );
-    cmd.AddValue ("capacidad", "Capacidad del bus", capacidad );
-    cmd.AddValue ("tamPaquete", "Tamaño de las SDU de aplicación", tamPaquete);
-    cmd.AddValue ("intervalo", "Tiempo entre dos paquetes consecutivos enviados por el mismo cliente", intervalo);
-
-	cmd.Parse (argc,argv);
-
-    NS_LOG_INFO ("Se han configurado los siguientes argumentos de entrada:" << std::endl <<
-                 "     -nCsma:           " << (unsigned int)nCsma  << std::endl <<
-                 "     -retardoProp:     " << retardoProp.GetDouble()/1e3 << "us" << std::endl <<
-                 "     -capacidad:       " << capacidad.GetBitRate()/1e6 << "Mbps" << std::endl <<
-                 "     -tamPaquete:      " << tamPaquete << "B" << std::endl <<
-                 "     -intervalo:       " << intervalo.GetDouble()/1e9 << "s" << std::endl);
-
-
-    // -----------Antes de simular, preparamos las graficas-------
-    Gnuplot grafIntentos;
-    grafIntentos.SetTitle ("Número medio de intentos para tx en función del número máximo de reintentos admisibles");
-    grafIntentos.SetLegend ("Número máximo de reintentos admisibles", 
-        "Numero medio de intentos para transmitir de los clientes");
-    Gnuplot2dDataset datosIntentos("intentos");
-    datosIntentos.SetStyle(Gnuplot2dDataset::LINES_POINTS);
-    datosIntentos.SetErrorBars(Gnuplot2dDataset::Y);
-
-    Gnuplot grafTiempo;
-    grafTiempo.SetTitle ("Tiempo medio hasta recepción del eco en función del número máximo de reintentos admisibles");
-    grafTiempo.SetLegend ("Número máximo de reintentos admisibles", 
-        "Tiempo medio hasta recepción del eco (ms)");
-    Gnuplot2dDataset datosTiempo("tiempo de eco");
-    datosTiempo.SetStyle(Gnuplot2dDataset::LINES_POINTS);
-    datosTiempo.SetErrorBars(Gnuplot2dDataset::Y);
-
-    Gnuplot grafPorcentaje;
-    grafPorcentaje.SetTitle ("Porcentaje de paquetes transmitidos en función del número máximo de reintentos admisibles");
-    grafPorcentaje.SetLegend ("Número máximo de reintentos admisibles", 
-        "Porcentaje de paquetes transmitidos correctamente (%)");
-    Gnuplot2dDataset datosPorcentaje("% pkts enviados");
-    datosPorcentaje.SetStyle(Gnuplot2dDataset::LINES_POINTS);
-    datosPorcentaje.SetErrorBars(Gnuplot2dDataset::Y);
-    // ---------------------------------------------------------------------------------------------- 
-
-    ResultSimulaciones result;
-
-    for (int maxReintentos = 1; maxReintentos <= 15; maxReintentos++)
-    {
-
-        NS_LOG_INFO("-----------------Comenzamos el bloque de simulaciones para maxReintentos = " << maxReintentos << "-----------------");
-        result=simulacion(nCsma, retardoProp, capacidad, tamPaquete, intervalo, maxReintentos);
-
-
-        //Si los resultados son correctos, los introducimos en la grafica
-        if(result.medias.intentos != -1 and result.varianzas.intentos != -1)
-        {
-            //Calculamos el intervalo de confianza
-            z = TSTUDENT10*sqrt(result.varianzas.intentos/(NUM_SIMULACIONES));
-            // Añadimos los datos para representar en la grafica
-            datosIntentos.Add(maxReintentos, result.medias.intentos, z);
-        }
-
-        if(result.medias.tiempo != -1 and result.varianzas.tiempo != -1)   
-        {
-            //Calculamos el intervalo de confianza
-            z = TSTUDENT10*sqrt(result.varianzas.tiempo/(NUM_SIMULACIONES));
-            // Añadimos los datos para representar en la grafica
-            datosTiempo.Add(maxReintentos, result.medias.tiempo, z);
-        }
-    
-        if(result.medias.porcentaje != -1 and result.varianzas.porcentaje != -1)
-        {
-            //Calculamos el intervalo de confianza
-            z = TSTUDENT10*sqrt(result.varianzas.porcentaje/(NUM_SIMULACIONES));
-            // Añadimos los datos para representar en la grafica
-            datosPorcentaje.Add(maxReintentos, result.medias.porcentaje, z);
-        }
-    }
-
-    //--------Introducimos los resultados en las graficas-----------------------------------
-    std::ofstream ficheroIntentos("practica05-01.plt");
-    grafIntentos.AddDataset(datosIntentos);
-    grafIntentos.GenerateOutput(ficheroIntentos);
-    ficheroIntentos << "pause -1" << std::endl;
-    ficheroIntentos.close();
-
-    std::ofstream ficheroTiempo("practica05-02.plt");
-    grafTiempo.AddDataset(datosTiempo);
-    grafTiempo.GenerateOutput(ficheroTiempo);
-    ficheroTiempo << "pause -1" << std::endl;
-    ficheroTiempo.close();
-
-    std::ofstream ficheroPorcentaje("practica05-03.plt");
-    grafPorcentaje.AddDataset(datosPorcentaje);
-    grafPorcentaje.GenerateOutput(ficheroPorcentaje);
-    ficheroPorcentaje << "pause -1" << std::endl;
-    ficheroPorcentaje.close();
-    //-------------------------------------------------  
-*/
-    return 0;
+return 0;
 }
-/*
-
-//Simulación simple para el servicio FTP usando CSMA
-void
-simulacionCSMA_FTP (uint32_t nCsma, Time retardoProp, DataRate capacidad, uint32_t tamPaquete)
-{
-    NS_LOG_FUNCTION(nCsma << retardoProp << capacidad << tamPaquete );
-
-    NodeContainer csmaNodes;
-    csmaNodes.Create (nCsma);
-
-    CsmaHelper csma;
-    csma.SetChannelAttribute ("DataRate", DataRateValue (capacidad));
-    csma.SetChannelAttribute ("Delay", TimeValue (retardoProp));
-
-    NetDeviceContainer csmaDevices;
-    csmaDevices = csma.Install (csmaNodes);
-    // Instalamos la pila TCP/IP en todos los nodos
-    InternetStackHelper stack;
-    stack.Install (csmaNodes);
-    // Y les asignamos direcciones
-    Ipv4AddressHelper address;
-    address.SetBase ("10.0.0.0", "255.255.255.0");
-    Ipv4InterfaceContainer csmaInterfaces = address.Assign (csmaDevices);            
-
-    // Cálculo de rutas
-    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-
-    /////////// Instalación de las aplicaciones
-    // Servidor
-    uint16_t port = 9;
-    PacketSinkHelper sink ("ns3::TcpSocketFactory",
-                     Address (InetSocketAddress (Ipv4Address::GetAny (), port)));
-    sink.SetAttribute ("Protocol", TypeIdValue (TcpSocketFactory::GetTypeId ()));
-    ApplicationContainer sinkApp = sink.Install (csmaNodes.Get (0));
-
-    sinkApp.Start (Seconds (1.0));
-    sinkApp.Stop (Seconds (10.0));
-
-    // Clientes
-    AddressValue remoteAddress (InetSocketAddress (csmaInterfaces.GetAddress(0,0), port));
-    //Config::SetDefault ("ns3::TcpSocket::SegmentSize", UintegerValue (tcp_adu_size));
-    BulkSendHelper ftp ("ns3::TcpSocketFactory", Address());
-    ftp.SetAttribute ("Remote", remoteAddress);
-    ftp.SetAttribute ("MaxBytes", UintegerValue (0));
-
-    //Contenedor de los nodos clientes
-    NodeContainer clientes;
-    for (uint32_t i = 1; i < nCsma; i++)
-        clientes.Add(csmaNodes.Get(i));
-
-    ApplicationContainer sourceApp = ftp.Install (clientes);
-    sourceApp.Start (Seconds (2.0));
-    sourceApp.Stop (Seconds (9.0));
-
-    // Suscribimos un objeto Observador a las trazas de cada nodo.
-    //Nodo 0 es el sumidero
-    ObservadorCSMA observadorCSMA;
-
-    for (uint32_t j = 1; j < nCsma; j++)
-    {
-        observadorCSMA.SetNodo((int)j);
-        observadorCSMA.SetTamPkt(tamPaquete);
-        //csmaDevices.Get(j)->TraceConnectWithoutContext ("MacTxBackoff", MakeCallback(&ObservadorCSMA::EnvioRetrasado, &observadorCSMA[j]));
-        csmaDevices.Get(j)->TraceConnectWithoutContext ("PhyTxDrop", MakeCallback(&ObservadorCSMA::EnvioDescartado, &observadorCSMA));
-        //csmaDevices.Get(j)->TraceConnectWithoutContext ("PhyTxEnd", MakeCallback(&ObservadorCSMA::EnvioTerminado, &observadorCSMA[j]));
-        //csmaDevices.Get(j)->TraceConnectWithoutContext ("MacTx", MakeCallback(&ObservadorCSMA::OrdenEnvio, &observadorCSMA[j]));
-        sourceApp.Get(j-1)->GetObject<BulkSendApplication>()->TraceConnectWithoutContext ("Tx", MakeCallback(&ObservadorCSMA::PktGenerado, &observadorCSMA));
-            
-        //Aprovechamos para cambiar el numero maximo de reintentos de tx 
-        //csmaDevices.Get(j)->GetObject<CsmaNetDevice>()->SetBackoffParams (Time ("1us"), 10, 1000, 10, maxReintentos);
-    }  
-
-    observadorCSMA.SetNodo((int)0);
-    observadorCSMA.SetTamPkt(tamPaquete);
-    sinkApp.Get(0)->GetObject<PacketSink>()->TraceConnectWithoutContext ("Rx", MakeCallback(&ObservadorCSMA::PktRecibido, &observadorCSMA));   
-
-    Simulator::Stop(Seconds(10.0));
-    Simulator::Run ();
-    Simulator::Destroy ();
-
-    NS_LOG_ERROR(observadorCSMA.GetMediaTiempos());
-    //NS_LOG_ERROR(observadorCSMA[157].GetMediaIntentos());
-    //NS_LOG_ERROR(observadorCSMA[157].GetPorcentajePktsRetrasados ());
-}*/
 
 
 //Simulación simple para el servicio VoIP usando CSMA
@@ -248,7 +45,7 @@ NS_LOG_FUNCTION(nCsma << ton << toff << sizePkt << dataRate << prob_error_pkt);
   Ptr<RateErrorModel> modelo_error = CreateObject<RateErrorModel> ();
 
   modelo_error->SetRate(prob_error_pkt);
-  modelo_error->SetUnit(RateErrorModel::ERROR_UNIT_PACKET);
+  modelo_error->SetUnit(RateErrorModel::ERROR_UNIT_BIT);
 
   // Nodos que pertenecen a la red de área local
   // Como primer nodo añadimos el sumidero.
@@ -311,10 +108,7 @@ NS_LOG_FUNCTION(nCsma << ton << toff << sizePkt << dataRate << prob_error_pkt);
   //Instalamos la aplicación On/Off en todos y cada uno de los nodos de la red de área local.
   ApplicationContainer clientApps = VoIP.Install (clientes);
   clientApps.Start (Seconds (2.0));
-  clientApps.Stop (Seconds (100.0));
-
-  // Activamos las trazas pcap en las dos interfaces del nodo de enlace
-  //csma.EnablePcap ("practica06", csmaDevices.Get (0), true);
+  clientApps.Stop (Seconds (20.0));
 
   //Objeto observador para obtener los resultados de las simulaciones.
   ObservadorCSMA observadorCSMA;
@@ -328,7 +122,6 @@ NS_LOG_FUNCTION(nCsma << ton << toff << sizePkt << dataRate << prob_error_pkt);
     
   for (uint32_t j = 1; j < nCsma; j++)
   {
-    csmaDevices.Get(j)->TraceConnectWithoutContext ("PhyTxDrop", MakeCallback(&ObservadorCSMA::EnvioDescartado, &observadorCSMA));
     //Aprovechamos para cambiar el numero maximo de reintentos de tx 
     csmaDevices.Get(j)->GetObject<CsmaNetDevice>()->SetBackoffParams (Time ("1us"), 10, 1000, 10, 8);
   }
